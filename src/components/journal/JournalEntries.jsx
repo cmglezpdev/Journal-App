e537bca8bcac8fbb5e2db6d74bab8eedce1b555e
Carlos Manuel Gonzalez <cmglezpdev@gmail.com>
2022-08-01 15:32:40 -0400

    sort entries by date

--- src/components/journal/JournalEntries.jsx
+++ src/components/journal/JournalEntries.jsx
@@ -4,6 +4,8 @@ import { JournalEntry } from "./JournalEntry";
 export const JournalEntries = () => {
 
     const { notes: entries } = useSelector(state => state.notes);
+    entries.sort((a, b) => a.date > b.date ? -1 : 1);
+
 
     return (
         <div className="journal__entries">
